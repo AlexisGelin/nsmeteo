cd3e6305966b04a42063569044578ff24396fc10
dyzzorka <83311421+dyzzorka@users.noreply.github.com>
2022-06-02 19:50:36 +0200

    db ok

---
+++ lib/db/myDB.dart
@@ -0,0 +1,34 @@
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class myDB {
+  static Future<Database> initDatabase() async {
+    var databasesPath = await getDatabasesPath();
+    String path = join(databasesPath, 'bob.db');
+    Database db = await openDatabase(path, version: 3,
+        onCreate: (Database db, int version) async {
+      await db.execute('''
+            CREATE TABLE IF NOT EXISTS City( 
+                  id primary key,
+                  name varchar(255) not null
+              );
+          ''');
+    });
+    return db;
+  }
+
+  static Future<List<Map>> getDB(Database db) async {
+    List<Map> list = await db.rawQuery('SELECT * FROM City');
+    return list;
+  }
+
+  static Future<void> insertDB(Database db, String cityName) async {
+    await db.transaction((txn) async {
+      await txn.rawInsert('INSERT INTO City(name) VALUES(?)', [cityName]);
+    });
+  }
+
+  static Future<void> deleteRecord(Database db, String city) async {
+    await db.rawDelete('DELETE FROM City WHERE name = ?', [city]);
+  }
+}
---
+++ lib/widgets/speakDb.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+import 'package:nsmeteo/db/myDB.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late Database db;
+  List<Map> list = [];
+  final controll = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration.zero, () async {
+      db = await myDB.initDatabase();
+      // await myDB.insertDB(db, "lyon");
+      // await myDB.insertDB(db, "paris");
+      // await myDB.insertDB(db, "marseille");
+      // await myDB.insertDB(db, "nice");
+      // await myDB.insertDB(db, "bordeaux");
+      // await myDB.deleteRecord(db, "lyon");
+      // await myDB.deleteRecord(db, "paris");
+      // await myDB.deleteRecord(db, "marseille");
+      // await myDB.deleteRecord(db, "nice");
+      // await myDB.deleteRecord(db, "bordeaux");
+
+      myDB.getDB(db).then((value) {
+        setState(() {
+          list = value;
+        });
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(),
+        body: Center(
+            child: Column(
+          children: [
+            TextField(
+              controller: controll,
+            ),
+            ElevatedButton(
+                child: const Text("City"),
+                onPressed: () {
+                  setState(() {
+                    myDB.insertDB(db, controll.text);
+                    myDB.getDB(db).then((value) {
+                      setState(() {
+                        list = value;
+                      });
+                    });
+                  });
+                }),
+            ListView.builder(
+              shrinkWrap: true,
+              scrollDirection: Axis.vertical,
+              itemBuilder: (context, index) {
+                return Container(
+                    child: Column(children: [
+                  Text(
+                    list[index]["name"],
+                    style: const TextStyle(fontSize: 25),
+                  ),
+                  IconButton(
+                      onPressed: () {
+                        setState(() {
+                          myDB.deleteRecord(db, list[index]["name"]);
+                          myDB.getDB(db).then((value) {
+                            setState(() {
+                              list = value;
+                            });
+                          });
+                        });
+                      },
+                      icon: const Icon(Icons.delete))
+                ]));
+              },
+              itemCount: list.length,
+            ),
+          ],
+        )));
+  }
+}
