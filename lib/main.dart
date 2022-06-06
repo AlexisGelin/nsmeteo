3bdd95b7f5ad66d343317c7f14ea7cab57e2b4c5
Alexis GELIN <alexis.gelin@ynov.com>
2022-06-07 01:35:48 +0200

    adding carousel

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:nsmeteo/utils/appTheme.dart';
 import 'package:nsmeteo/widgets/Block.dart';
@@ -15,10 +16,9 @@ class MyApp extends StatelessWidget {
       theme: buildTheme(context),
       initialRoute: '/ville',
       routes: {
-        // When navigating to the "/" route, build the FirstScreen widget.
         '/ville': (context) => const OneCityScreen(),
-        // When navigating to the "/second" route, build the SecondScreen widget.
         '/menu': (context) => const SecondScreen(),
+        '/carrousel': (context) => FullscreenSliderDemo(),
       },
     );
   }
@@ -44,6 +44,10 @@ class _OneCityScreenState extends State<OneCityScreen> {
       if (_selectedIndex == 2) {
         _selectedIndex = 1;
         Navigator.pushNamed(context, '/menu');
+      }
+      if (_selectedIndex == 0) {
+        _selectedIndex = 1;
+        Navigator.pushNamed(context, '/carrousel');
       }
     });
   }
@@ -207,6 +211,50 @@ class SecondScreen extends StatelessWidget {
             ListTile(),
           ],
         ),
+      ),
+    );
+  }
+}
+
+final List<String> imgList = [
+  'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
+  'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
+  'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
+  'https://images.unsplash.com/photo-1523205771623-e0faa4d2813d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89719a0d55dd05e2deae4120227e6efc&auto=format&fit=crop&w=1953&q=80',
+  'https://images.unsplash.com/photo-1508704019882-f9cf40e475b4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8c6e5e3aba713b17aa1fe71ab4f0ae5b&auto=format&fit=crop&w=1352&q=80',
+  'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80'
+];
+
+class FullscreenSliderDemo extends StatelessWidget {
+  const FullscreenSliderDemo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Ville',
+      style: Theme.of(context).textTheme.titleSmall)),
+      body: Builder(
+        builder: (context) {
+          final double height = MediaQuery.of(context).size.height;
+          return CarouselSlider(
+            options: CarouselOptions(
+              height: height,
+              viewportFraction: 1.0,
+              enlargeCenterPage: false,
+              // autoPlay: false,
+            ),
+            items: imgList
+                .map((item) => Container(
+                      child: Center(
+                          child: Image.network(
+                        item,
+                        fit: BoxFit.cover,
+                        height: height,
+                      )),
+                    ))
+                .toList(),
+          );
+        },
       ),
     );
   }
